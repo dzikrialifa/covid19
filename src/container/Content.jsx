89a054fbe0c46193bdeb41c93796375972759417
dzikrialifa <dzikrialif99@gmail.com>
2020-03-31 14:47:52 +0700

    Content page

--- src/container/Content.jsx
+++ src/container/Content.jsx
@@ -1,6 +1,6 @@
-import React,{Component} from 'react';
+import React from 'react';
 
-export default class Content extends Component {
+export default class Content extends React.Component {
 
     state = {
         dataIndonesia: [{}]
