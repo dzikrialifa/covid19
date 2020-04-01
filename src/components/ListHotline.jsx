9b7308b98afda791e40834470a1ce01ad4104646
dzikrialifa <dzikrialif99@gmail.com>
2020-04-01 22:02:39 +0700

    Receive List hotlines

--- src/components/ListHotline.jsx
+++ src/components/ListHotline.jsx
@@ -4,8 +4,8 @@ const ListHotline = props => {
     return (
             <div className="card provinsi mt-3" style={{height:"130px"}}>
                 <div className="card-body">
-                    <h5 className="text-capitalize text-center">{props.name}</h5>
-                    <h6>+62{props.telp}</h6>
+                    <h6 className="text-capitalize text-center pt-3">{props.name}</h6>
+                    <h6 className="text-center">{props.telp}</h6>
                 </div>
             </div>
     )
